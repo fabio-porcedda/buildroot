2b03649236220e77e69e80c42fa8ed0b69a73d55
Fabio Porcedda <fabio.porcedda@gmail.com>
2015-02-22 22:15:33 +0100

    erlang-p1-zlib: fix cflags

--- package/erlang-p1-zlib/erlang-p1-zlib.mk
+++ package/erlang-p1-zlib/erlang-p1-zlib.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-ERLANG_P1_ZLIB_VERSION = 52e82bb
-ERLANG_P1_ZLIB_SITE = $(call github,processone,zlib,$(ERLANG_P1_ZLIB_VERSION))
+ERLANG_P1_ZLIB_VERSION = a10356b28eff3ff7080e1559fd0e43c525993606
+ERLANG_P1_ZLIB_SITE = $(call github,fabio-porcedda,zlib,$(ERLANG_P1_ZLIB_VERSION))
 ERLANG_P1_ZLIB_LICENSE = GPLv2+
 ERLANG_P1_ZLIB_LICENSE_FILES = COPYING
 ERLANG_P1_ZLIB_DEPENDENCIES = zlib
