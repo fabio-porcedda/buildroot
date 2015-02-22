9cc6bb41e1f4b933fbbcec2c31a443c01c97ac27
Fabio Porcedda <fabio.porcedda@gmail.com>
2015-02-22 22:15:33 +0100

    erlang-p1-zlib: bump to a version that use TARGET_CFLAGS
    This is in order to support the per-package staging directory.
    
    Signed-off-by: Fabio Porcedda <fabio.porcedda@gmail.com>
    Cc: Johan Oudinet <johan.oudinet@gmail.com>

--- package/erlang-p1-zlib/erlang-p1-zlib.mk
+++ package/erlang-p1-zlib/erlang-p1-zlib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLANG_P1_ZLIB_VERSION = 52e82bb
+ERLANG_P1_ZLIB_VERSION = 513c7e52a01616e7af74c67ac246dc09a3c071e4
 ERLANG_P1_ZLIB_SITE = $(call github,processone,zlib,$(ERLANG_P1_ZLIB_VERSION))
 ERLANG_P1_ZLIB_LICENSE = GPLv2+
 ERLANG_P1_ZLIB_LICENSE_FILES = COPYING
