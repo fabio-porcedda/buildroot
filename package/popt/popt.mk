a075f35b50d4a668a93d3b4025436268b9d5fcf8
Fabio Porcedda <fabio.porcedda@gmail.com>
2015-02-24 08:46:24 +0100

    popt: add to the "popt.pc" the dependency libintl
    This change permits through pkg-config to retrive even the depenencies
    that is useful for a static linking.
    
    Signed-off-by: Fabio Porcedda <fabio.porcedda@gmail.com>

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -15,9 +15,18 @@ POPT_GETTEXTIZE = YES
 
 POPT_CONF_ENV = ac_cv_va_copy=yes
 
+define POPT_POST_PATCH_FIX_PC
+	$(SED) "s/Libs:/Libs.private: -lintl\nLibs:/" $(@D)/popt.pc.in
+endef
+
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 POPT_CONF_ENV += am_cv_lib_iconv=yes
 POPT_CONF_OPTS += --with-libiconv-prefix=$(STAGING_DIR)/usr
+endif
+
+ifeq ($(BR2_PACKAGE_GETTEXT),y)
+ POPT_DEPENDENCIES += gettext
+ POPT_POST_PATCH_HOOKS += POPT_POST_PATCH_FIX_PC
 endif
 
 $(eval $(autotools-package))
