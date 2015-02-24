7fea3bf2247f34b0e278886afc6197f866494616
Fabio Porcedda <fabio.porcedda@gmail.com>
2015-02-24 19:04:11 +0100

    logrotate temp

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -10,15 +10,6 @@ LOGROTATE_LICENSE = GPLv2+
 LOGROTATE_LICENSE_FILES = COPYING
 
 LOGROTATE_DEPENDENCIES = popt
+LOGROTATE_AUTORECONF = YES
 
-define LOGROTATE_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC) $(TARGET_CFLAGS)" LDFLAGS="$(LDFLAGS)" -C $(@D)
-endef
-
-define LOGROTATE_INSTALL_TARGET_CMDS
-	$(MAKE) PREFIX=$(TARGET_DIR) -C $(@D) install
-	$(INSTALL) -m 0644 package/logrotate/logrotate.conf $(TARGET_DIR)/etc/logrotate.conf
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/logrotate.d
-endef
-
-$(eval $(generic-package))
+$(eval $(autotools-package))
